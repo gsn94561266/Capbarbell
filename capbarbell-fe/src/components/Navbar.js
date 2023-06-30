0ceca147a9c95c7d861240a9be5c1cbae46a14c1
Tony <gsn94561266@gmail.com>
2023-06-30 14:30:41 +0800

    delete Navbar.scss

--- capbarbell-fe/src/components/Navbar.js
+++ capbarbell-fe/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Navbar.scss';
 // 圖示
 import { MdOutlinePeopleAlt, MdOutlineDescription } from 'react-icons/md';
 
